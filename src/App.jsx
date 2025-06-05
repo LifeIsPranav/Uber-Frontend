cdf98c41e2abd87146e4d5af756ac33353805946
Pranav <lifeispranav@gmail.com>
2025-06-05 17:41:27 +0530

    Fix: Fixed Routing imports and created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Route, Routes } from 'react-router-dom'
 
+import './App.css'
+
+import Home from './pages/Home'
+import CaptainRegister from './pages/CaptainRegister'
+import CaptainLogin from './pages/CaptainLogin'
+import UserRegister from './pages/UserRegister'
+import UserLogin from './pages/UserLogin'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
